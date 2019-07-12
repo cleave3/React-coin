8baf5493dc0edbf241385ea25ced751748f2888f
cleave3 <owhiroroeghele@gmail.com>
2019-07-12 05:28:52 +0100

    create app build for production

---
+++ build/precache-manifest.484ad64ad416dd82af2dc090a1b32bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e44a7136db257ec5f35e84eb2405f28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8bde1dd0f2bc2ff52df",
+    "url": "/static/css/main.58b75e64.chunk.css"
+  },
+  {
+    "revision": "596d9721782645d46c8f",
+    "url": "/static/js/2.bc76c705.chunk.js"
+  },
+  {
+    "revision": "e8bde1dd0f2bc2ff52df",
+    "url": "/static/js/main.41aff1f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/static/media/logo.5c0436d5.png"
+  }
+]);
